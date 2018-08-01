8dca4ad189beebc0a9e730bea73282e99c8f2d13
Sancho Montana <spencersnider9000@gmail.com>
2018-07-31 20:08:53 -0400

    Implemented a moving background.

--- FlappyBird.py
+++ FlappyBird.py
@@ -5,9 +5,11 @@ DISPLAY_WIDTH = 1200
 DISPLAY_HEIGHT = 900
 gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 clock = pygame.time.Clock()
-
 FPS = 50 # Frames per second.
-
+background = pygame.image.load("res/forest.jpg")
+background = pygame.transform.scale(background, (DISPLAY_WIDTH, DISPLAY_HEIGHT))
+rect = background.get_rect()
+distance = 0
 # Main loop:
 game_exit = False
 while not game_exit:
@@ -15,9 +17,15 @@ while not game_exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_exit = True
-
+    
+    distance += 2
+    # Moving Background
+    rect = rect.move((-distance, 0))
+    gameDisplay.blit(background, (rect[0] % DISPLAY_WIDTH, rect[1]))
+    gameDisplay.blit(background, (rect[0] % DISPLAY_WIDTH - DISPLAY_WIDTH, rect[1]))
+    
     pygame.display.update()
     clock.tick(FPS)
-
+    
 
 quit()
