b04231c9b30049e236f2774571b1d1daf6d852f5
Sancho Montana <spencersnider9000@gmail.com>
2018-07-31 22:05:16 -0400

    Fixed all merge conflicts.

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,69 +1,48 @@
-import pygame
-from Bird import Bird
-
-pygame.init()
+import pygame                                                                   
+from Bird import Bird                                                           
+                                                                                
+pygame.init()                                                                   
 ball = pygame.image.load("res/ball.bmp") # This loads the player into existance.
-pygame.mouse.set_visible(False)
-
-DISPLAY_WIDTH = 1200
-DISPLAY_HEIGHT = 900
-gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
-background = pygame.image.load("res/forest.jpg")
+pygame.mouse.set_visible(False)                                                 
+                                                                                
+DISPLAY_WIDTH = 1200                                                            
+DISPLAY_HEIGHT = 900                                                            
+gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))          
+background = pygame.image.load("res/forest.jpg")                                
 background = pygame.transform.scale(background, (DISPLAY_WIDTH, DISPLAY_HEIGHT))
-rect = background.get_rect()
-
-clock = pygame.time.Clock()
-FPS = 50 # Frames per second.
-
-player = Bird(DISPLAY_HEIGHT / 2)
-
-distance = 0
-
-time = 0
-currentVerticalPos = 450
-gravity = 1.09
-
-# Image init:
-ball = pygame.image.load("ball.bmp")
-ballrect = ball.get_rect()
-
-# Main loop:
-game_exit = False
-while not game_exit:
-    print(currentVerticalPos)
-    # Event handling.
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_exit = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                time = 0
-                player.jump()
-
-    # Moving Background.
-    distance += 2
-    rect[0] = -distance
-    gameDisplay.blit(background, (rect[0] % DISPLAY_WIDTH, rect[1]))
+rect = background.get_rect()                                                    
+                                                                                
+clock = pygame.time.Clock()                                                     
+FPS = 50 # Frames per second.                                                   
+                                                                                
+player = Bird(DISPLAY_HEIGHT / 2)                                               
+                                                                                
+distance = 0                                                                    
+# Main loop:                                                                    
+game_exit = False                                                               
+while not game_exit:                                                            
+    # Event handling.                                                           
+    for event in pygame.event.get():                                            
+        if event.type == pygame.QUIT:                                           
+            game_exit = True                                                    
+        if event.type == pygame.KEYDOWN:                                        
+            if event.key == pygame.K_SPACE:                                     
+                player.jump()                                                   
+                                                                                
+    # Moving Background.                                                        
+    distance += 2                                                               
+    rect[0] = -distance                                                         
+    gameDisplay.blit(background, (rect[0] % DISPLAY_WIDTH, rect[1]))            
     gameDisplay.blit(background, (rect[0] % DISPLAY_WIDTH - DISPLAY_WIDTH, rect[1]))
-
-    if currentVerticalPos > DISPLAY_HEIGHT:
-        sys.exit()
-    currentVerticalPos += time - 15
-                            # Middle-ish of screen
-    gameDisplay.blit(ball, (450, currentVerticalPos))
-        
-    pygame.display.update()
-    clock.tick(FPS)
-    time += 1
-   
-    # Moving the player.
-    player.fall()
-    player.time += 1
-    gameDisplay.blit(ball, (50, player.position))
-    
-    # Frame by frame updating.
-    pygame.display.update()
-    clock.tick(40)
-
+                                                                                
+    # Moving the player.                                                        
+    player.fall()                                                               
+    player.time += 1                                                            
+    gameDisplay.blit(ball, (50, player.position))                               
+                                                                                
+    # Frame by frame updating.                                                  
+    pygame.display.update()                                                     
+    clock.tick(40)                                                              
+                                                                                
+                                                                                
 quit()
-
