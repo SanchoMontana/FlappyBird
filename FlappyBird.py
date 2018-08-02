68898b3ecad607ec8b70ce5cf77378462b9fc98a
Sancho Montana <spencersnider9000@gmail.com>
2018-08-02 14:35:12 -0400

    Bird picture is now a png file and cropped to its exact size.

--- FlappyBird.py
+++ FlappyBird.py
@@ -18,14 +18,14 @@ background = pygame.transform.scale(background, (DISPLAY_WIDTH, DISPLAY_HEIGHT))
 rect = background.get_rect()
 
 clock = pygame.time.Clock()
-FPS = 30 # Frames per second.  --- Faster, yet smaller moving increments
+FPS = 30 # Frames per second.
 
 pipes = []
 elapsed_time = 0
 prev_click = 0
 player = Bird(DISPLAY_HEIGHT, ball.get_rect()[3])
 distance = 0
-cid = 0
+# cid = 0 ...will be used a litlle later (change in distance)
 # Main loop:
 game_exit = False
 while not game_exit:
@@ -43,21 +43,6 @@ while not game_exit:
     gameDisplay.blit(background, (rect[0] % DISPLAY_WIDTH, rect[1]))
     gameDisplay.blit(background, (rect[0] % DISPLAY_WIDTH - DISPLAY_WIDTH, rect[1]))
 
-    # Moving the player.
-    player.fall()
-    player.prev_click = player.time
-    player.time += 1
-
-    ### Rotate bird
-    #prev_cid = cid
-    #cid = player.prev_position - player.position 
-    #print(cid)
-    #print(rad)
-    #ball = pygame.transform.rotate(ball, )
-    
-
-    gameDisplay.blit(ball, (50, player.position))
-
     # Pipe logic.
     if elapsed_time % (FPS*1.5) == 0:
         pipes.insert(0, Pipe(gameDisplay, DISPLAY_HEIGHT, DISPLAY_WIDTH, 6, (20, 130, 200)))
@@ -66,6 +51,19 @@ while not game_exit:
         if pipe.x_pos < -pipe.width:
             del pipe
             del pipes[-1]
+
+    # Moving the player.
+    player.fall()
+    player.prev_click = player.time
+    player.time += 1
+
+    # Rotate the player.
+    #prev_cid = cid
+    #cid = player.prev_position - player.position 
+    #print(cid)
+    #print(rad)
+    #ball = pygame.transform.rotate(ball, )
+    gameDisplay.blit(ball, (50, player.position))
 
     # Frame by frame updating.
     pygame.display.update()
