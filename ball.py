9471b2feff710def10ae5d4ebcaa7ea7e1181ae3
oesa77soccer <oesa77soccer@gmail.com>
2018-08-01 22:29:28 -0400

    Added better mechanic to ball.py for bird

--- ball.py
+++ ball.py
@@ -6,7 +6,7 @@ DISPLAY_WIDTH = 1200
 DISPLAY_HEIGHT = 700
 gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 clock = pygame.time.Clock()
-FPS = 50 # Frames per second.
+FPS = 100 # Frames per second.
 background = pygame.image.load("res/forest.jpg")
 background = pygame.transform.scale(background, (DISPLAY_WIDTH, DISPLAY_HEIGHT))
 back = background.get_rect()
@@ -17,10 +17,11 @@ time = 0
 currentVerticalPos = DISPLAY_HEIGHT / 2
 
 h_b_r = 230
+speed = 2
 
 rectangleObjectList = []
 black = 0, 0, 0
-
+prev_click = 0
 # Image init:
 
 ball = pygame.image.load("res/smallbird.bmp")
@@ -40,41 +41,47 @@ def displayRectangles():
         pygame.draw.rect(gameDisplay, black, [x, gap_pos, w, DISPLAY_HEIGHT - gap_pos])
 
 # Main loop:
-game_exit = False
-while not game_exit:
-    # Event handling.
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_exit = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                click_time = 0
-    
-    distance += 1
-    # Moving Background
-    back[0] = -distance
-    gameDisplay.blit(background, (back[0] % DISPLAY_WIDTH, back[1]))
-    gameDisplay.blit(background, (back[0] % DISPLAY_WIDTH - DISPLAY_WIDTH, back[1]))
-
-    if currentVerticalPos > DISPLAY_HEIGHT:
-        sys.exit()
-    if time % (FPS*3) == 0:
-        rectangleObjectList.append(Pipe(DISPLAY_HEIGHT, DISPLAY_WIDTH, 2))
-
-    # iterate each rect object
-    for rect in rectangleObjectList:
-        rect.move()
-        if rect.x_pos < -rect.pipe_width:
-            rectangleObjectList.pop(0)
-    displayRectangles()
-
-    currentVerticalPos += click_time - 13
-                            # Middle-ish of screen
-    gameDisplay.blit(ball, (450, currentVerticalPos))
+while True:
+    game_exit = False
+    while not game_exit:
+        # Event handling.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_exit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    click_time = 0
         
-    pygame.display.update()
-    clock.tick(FPS)
-    click_time += 1
-    time += 1 
+        distance += 1
+        # Moving Background
+        back[0] = -distance
+        gameDisplay.blit(background, (back[0] % DISPLAY_WIDTH, back[1]))
+        gameDisplay.blit(background, (back[0] % DISPLAY_WIDTH - DISPLAY_WIDTH, back[1]))
+
+        if currentVerticalPos > DISPLAY_HEIGHT:
+            sys.exit()
+            #game_exit = True
+        if time % (FPS) == 0:
+            rectangleObjectList.append(Pipe(DISPLAY_HEIGHT, DISPLAY_WIDTH, speed))
+
+        # iterate each rect object
+        for rect in rectangleObjectList:
+            rect.move()
+            if rect.x_pos < -rect.pipe_width:   
+                rectangleObjectList.pop(0)
+        displayRectangles()
+
+        currentVerticalPos += click_time - 20 + (prev_click * 1.3)
+        #print(click_time)
+                                # Middle-ish of screen
+        gameDisplay.blit(ball, (450, currentVerticalPos))
+            
+        pygame.display.update() 
+        clock.tick(FPS)
+        prev_click = click_time
+        click_time += 1
+        time += 1
+
+        #print(rectangleObjectList)
 
 quit()
